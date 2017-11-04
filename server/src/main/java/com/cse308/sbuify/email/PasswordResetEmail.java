9a7012699862576fd4d773d534459b7f6e1acb6b
Yun Joon Soh <yunjoon.soh@gmail.com>
2017-11-04 14:50:25 -0400

    Dummy Email Package

---
+++ server/src/main/java/com/cse308/sbuify/email/PasswordResetEmail.java
@@ -0,0 +1,12 @@
+package com.cse308.sbuify.email;
+
+import com.cse308.sbuify.user.User;
+
+//TODO
+public class PasswordResetEmail extends Email {
+	private User user;
+
+	public PasswordResetEmail() {
+
+	}
+}
