8ea5a4cc2a1cee669785aa82be9d26af2009ca24
Brett Porcelli <brettporcelli@gmail.com>
2017-11-15 21:53:47 -0500

    Tiny, sub-skeletal PlaylistController implementation.

--- server/src/main/java/com/cse308/sbuify/playlist/PlaylistController.java
+++ server/src/main/java/com/cse308/sbuify/playlist/PlaylistController.java
@@ -1,4 +1,16 @@
 package com.cse308.sbuify.playlist;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/api/playlists")
 public class PlaylistController {
+    /**
+     * Update a playlist.
+     */
+    @PatchMapping(path = "/{id}")
+    public void update(@PathVariable Integer id, @RequestBody Playlist playlist) {
+        // todo
+    }
 }
