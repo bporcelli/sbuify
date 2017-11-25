e744359e1d5cf90b192c8b5c58194e37e58dc3d5
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2017-11-25 17:29:53 -0500

    Implemented: Get all Songs admin endpoint with Test

--- server/src/main/java/com/cse308/sbuify/song/SongController.java
+++ server/src/main/java/com/cse308/sbuify/song/SongController.java
@@ -1,10 +1,46 @@
 package com.cse308.sbuify.song;
 
 
+import com.cse308.sbuify.common.TypedCollection;
+import com.cse308.sbuify.security.AuthFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/api/songs")
 public class SongController {
+
+    @Autowired
+    private SongRepository songRepository;
+
+    @Autowired
+    private AuthFacade authFacade;
+
+    /**
+     *  Get all song from db
+     * @return TypeCollection wrapper for the list of all songs
+     */
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public @ResponseBody TypedCollection getAllSongs(){
+
+        Iterable<Song> dbSongs = songRepository.findAll();
+
+        List<Song> songs = new ArrayList<>();
+        for (Song song: dbSongs) {
+            songs.add(song);
+        }
+
+        return new TypedCollection(songs, Song.class);
+
+    }
 }
--- server/src/test/java/com/cse308/sbuify/test/AdminControllerTest.java
+++ server/src/test/java/com/cse308/sbuify/test/AdminControllerTest.java
@@ -1,19 +1,20 @@
 package com.cse308.sbuify.test;
 
 import com.cse308.sbuify.song.Song;
+import com.cse308.sbuify.song.SongRepository;
 import com.cse308.sbuify.test.helper.AuthenticatedTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.xml.ws.Response;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -21,28 +22,32 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SongTest extends AuthenticatedTest {
+public class AdminControllerTest extends AuthenticatedTest {
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @Autowired
+    private SongRepository songRepository;
 
-	/**
-	 * Test: is search song work properly?
-	 */
-     @Test
-     public void postCustomerSong() {
-         // todo: update to use demo data
-         // prepare song to send
-//         Song toSend = new Song();
-//         toSend.setLength(30000);
-//
-//         System.out.println(toSend);
-//
-//         ResponseEntity<Void> response = restTemplate.postForEntity("http://localhost:" + port + "/api/customer/songs", toSend,
-//				Void.class);
-//
-//         assertEquals(HttpStatus.OK, response.getStatusCode());
-	}
+    /**
+     *  Test to get all songs from endpoint admin endpoint /api/songs/
+     */
+    @Test
+    public void getAllSongs(){
+
+        ResponseEntity<List<Song>> res = restTemplate.exchange("http://localhost:" + port + "/api/songs",  HttpMethod.GET,null, new ParameterizedTypeReference<List<Song>>() {
+        });
+        assertEquals(HttpStatus.OK, res.getStatusCode());
+        List<Song> songs = res.getBody();
+        assertNotNull(songs);
+
+        Iterable<Song> allSongs = songRepository.findAll();
+        List<Song> allSongRepo = new ArrayList<>();
+        for (Song song : allSongs){
+            allSongRepo.add(song);
+        }
+        assertEquals(allSongRepo.size(), songs.size());
+
+    }
+
     @Override
     public String getEmail() {
         return "sbuify+admin@gmail.com";  // use the user sbuify+admin@gmail.com for all tests in this class request require admin role
