c3067318987ebe141be57437c86ae1e2bbd623e1
Brett Porcelli <brettporcelli@gmail.com>
2017-11-25 16:04:03 -0500

    Remove unused findByKeyword methods.

--- server/src/main/java/com/cse308/sbuify/playlist/PlaylistRepository.java
+++ server/src/main/java/com/cse308/sbuify/playlist/PlaylistRepository.java
@@ -1,10 +1,6 @@
 package com.cse308.sbuify.playlist;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface PlaylistRepository extends CrudRepository<Playlist, Integer> {
-    @Query("FROM Playlist s WHERE LOWER(s.name) = LOWER(:keyword)")
-    Iterable<Playlist> findByKeyword(@Param("keyword") String keyword);
 }
