7e75688b8ec6b8b76146494fedc9a943b78a72da
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2017-11-27 20:51:09 -0500

    Updated: song to handle a generalized partial update

--- server/src/main/java/com/cse308/sbuify/song/SongController.java
+++ server/src/main/java/com/cse308/sbuify/song/SongController.java
@@ -3,10 +3,9 @@ package com.cse308.sbuify.song;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -21,23 +20,43 @@ public class SongController {
      * Activate or deactivate a song.
      *
      * @param songId
-     * @return HTTP.OK when song is successfully activated/deactivated, HTTP.NOT_FOUND - id not found
+     * @return HTTP.OK when song is successfully updated with partial song, HTTP.NOT_FOUND - id not found
      */
     @PatchMapping(path = "/{id}")
-    public ResponseEntity<?> activateDeactivateSong(@PathVariable("id") String songId) {
-        // TODO: Use a different endpoint or rewrite to handle generic updates to songs (not just activation/deactivation)
-        Optional<Song> foundSong = songRepository.findById(Integer.valueOf(songId));
-
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> updateSong(@PathVariable Integer songId, @RequestBody Song partialSong) {
+        Optional<Song> foundSong = songRepository.findById(songId);
         if (!foundSong.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         Song song = foundSong.get();
 
-        boolean newActiveStatus = !song.isActive();
-        song.setActive(newActiveStatus);
+        if (partialSong.getAlbum() != null){
+            song.setAlbum(partialSong.getAlbum());
+        }
+        if (partialSong.getFeaturedArtists() != null){
+            song.setFeaturedArtists(partialSong.getFeaturedArtists());
+        }
+        if (partialSong.getGenres() != null){
+            song.setGenres(partialSong.getGenres());
+        }
+        if (partialSong.getLength() != null){
+            song.setLength(partialSong.getLength());
+        }
+        if (partialSong.getLyrics() != null){
+            song.setLyrics(partialSong.getLyrics());
+        }
+        if (partialSong.getMBID() != null){
+            song.setMbid(partialSong.getMbid());
+        }
+        if (partialSong.getPlayCount() != null){
+            song.setPlayCount(partialSong.getPlayCount());
+        }
+        if (partialSong.getTrackNumber() != null){
+            song.setTrackNumber(partialSong.getTrackNumber());
+        }
         songRepository.save(song);
-
         return new ResponseEntity<>(song,HttpStatus.OK);
     }
 }
