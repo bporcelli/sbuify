76b22f1d0ffa4c99285bc674579e0ce8380e9226
Brett Porcelli <brettporcelli@gmail.com>
2017-12-05 14:29:21 -0500

    Move RoyaltyController to more appropriate package.

--- server/src/main/java/com/cse308/sbuify/label/RoyaltyController.java
+++ server/src/main/java/com/cse308/sbuify/label/RoyaltyController.java
@@ -1,4 +1,4 @@
-package com.cse308.sbuify.common;
+package com.cse308.sbuify.label;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
