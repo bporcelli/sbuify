6245d77822ab313715f716f2e178b82ba3cbaf7a
Brett Porcelli <brettporcelli@gmail.com>
2018-01-04 18:23:50 -0500

    Clean up a bit.

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-HOME=http://localhost:8080
-curl -X GET $HOME/admins
-echo ""
-curl -X POST -H "Content-Type: application/json" -d "{\"email\":\"asdf@asdf.com\",\"hashPassword\":\"pwdpwdpwd\",\"token\":\"tokenitis\",\"firstName\":\"Daniel\",\"lastName\":\"Soh\",\"isSuperAdmin\":true }" $HOME/admins/
-echo ""
-curl -X GET $HOME/admins
-echo ""
-curl -X GET -H "Content-Type: application/json" -d "{\"email\":\"asdf@asdf.com\",\"hashPassword\":\"pwdpwdpwd\"}" $HOME/log/in
